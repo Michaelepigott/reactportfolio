ab766d83fa803f5ccdde112ad99784bf962e8fe9
Michaelepigott <michaelepigott@gmail.com>
2023-12-15 16:43:15 -0500

    readme and content added

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # reactportfolio
+
+## Description
+A multi page personal portfolio created for personal promotion and practice using React libraries.  Includes an 'about me' page including a bio and portrait, a 'contact me' page including a form to send information including name and email (actually sending message to a DB is currently relegated to future development)  form validates that feilds are filled and that email feild is filled with an email, and a 'My Resume' page including bullet points of competencies and a download link for resume.
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+
+## Installation
+Open in browser.
+Deployed at: 
+
+## Usage
+
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+![about page](assets/images/aboutpage.png)
+ Image and bio provided, navigate site via navbar items with page names.
+
+ ![contact page](assets/images/contactpage.png)
+ Send information via entering name, email and message into labeled feilds.  Emails must be formatted as 'filler@xyz.etc' 
+ and all feilds must be filled
+
+ ![portfolio page](assets/images/portfoliopage.png)
+ Read descriptions and see images of developed applications, buttons to side link to github Repos and deployed applications.
+
+ ![resume page](assets/images/resumepage.png)
+ Read bullet points of personal competencies, clicking link downloads resume.
+
+## Credits
+
+troubleshooting advice, making buttons open external tabs and duel state variable advice: https://chat.openai.com/
+
+Adapted base code: edX bootcamp
+##
+
--- src/pages/portfoliopage.jsx
+++ src/pages/portfoliopage.jsx
@@ -2,9 +2,12 @@
 import React from 'react';
 import '../css/Body.css';
 import { Card } from 'react-bootstrap';
-import Recipieimage from '../assets/images/recipieproject.png'
-import Drinkimg from '../assets/images/drinkologist.png'
-
+import Recipieimage from '../assets/images/recipieproject.png';
+import Drinkimg from '../assets/images/drinkologist.png';
+import Portfol from '../assets/images/oldportfolio.png';
+import Schedimg from '../assets/images/Scheduling.png';
+import Quizimg from '../assets/images/quizImg.png';
+import Passgenimg from '../assets/images/passwordgen.png';
 //Create template for project cards
 const CardFormat = ( {title, info,image, link, git}) =>{
     return(
@@ -53,11 +56,32 @@ const projects = [
         link:'https://michaelepigott.github.io/Group-Project-8-25/',
     },
     {
-        title:'Drinkologist',
-        info:'A pure front end use of APIs to randomly generate and display a cocktail order based on user inputted ingredients',
-        git:'https://github.com/Michaelepigott/Group-Project-8-25', 
-        image: Drinkimg, 
-        link:'https://michaelepigott.github.io/Group-Project-8-25/',
+        title:'Previous Portfolio',
+        info:'A previous portfolio created as a project early in my learning process, shown here as a benchmark of improvement',
+        git:'https://github.com/Michaelepigott/Personal-Portfolio', 
+        image: Portfol, 
+        link:'https://michaelepigott.github.io/Personal-Portfolio/',
+    },
+    {
+        title:'Workday Scheduler',
+        info:'Hourly manager created using Day.js and local storage',
+        git:'https://michaelepigott.github.io/Hourlymangagerproject/', 
+        image: Schedimg, 
+        link:'https://github.com/Michaelepigott/Hourlymangagerproject',
+    },
+    {
+        title:'StudyQuiz',
+        info:'A coding study quiz created for practice with Dom traverse in JS',
+        git:'https://github.com/Michaelepigott/QuizProject', 
+        image: Quizimg, 
+        link:'https://michaelepigott.github.io/QuizProject/index.html',
+    },
+    {
+        title:'PasswordGenerator',
+        info:'Basic JS practice that autogenerates a password based on user input',
+        git:'https://github.com/Michaelepigott/Password-Generator', 
+        image: Passgenimg, 
+        link:'https://michaelepigott.github.io/Password-Generator/',
     },
 ];
 //Impliment push Project objects into template
